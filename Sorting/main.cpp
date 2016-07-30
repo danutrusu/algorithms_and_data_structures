73b57e465fa46c4dff3aaaacf3655726b137750e
Danut Rusu <danutmihaitarusu@gmail.com>
2016-07-30 14:30:57 +0200

    Added mergesort

---
+++ Sorting/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+void print_arr(int *arr,int n);
+int* read_arr(int length);
+
+//--------- mergesort start-----------//
+void merge(int *arr,int left,int mid,int right)
+{
+    int i = left, j = mid + 1, index = 0, *help_arr = new int [ right - left + 1];
+
+    for(int ind = 0; ind < right - left + 1; ++ind) {
+        help_arr[ind] = arr[left + ind];
+    }
+
+    while (i<=mid && j<=right) {
+        if ( arr[i] < arr[j] ) {
+            help_arr[index++] = arr[i++];
+        }
+        else {
+            help_arr[index++] = arr[j++];
+        }
+    }
+
+    while( i <= mid) {
+        help_arr[index++] = arr[i++];
+    }
+    while(j <= right) {
+        help_arr[index++] = arr[j++];
+    }
+
+    for(int ind = 0; ind < right - left + 1; ++ind) {
+        arr[left+ind] = help_arr[ind];
+    }
+
+}
+
+void mergesort(int *arr, int left, int right)
+{
+    if (left < right) {
+        int mid = (left+right) / 2;
+        mergesort(arr,left, mid);
+        mergesort(arr, mid+1, right);
+        merge(arr,left,mid,right);
+    }
+}
+void mergesort(int *arr, int n)
+{
+    mergesort(arr,0,n);
+}
+//--------- mergesort stop-----------//
+
+int main() {
+    int *array, length;
+    cin >> length;
+    array = read_arr(length);
+    mergesort(array, length - 1);
+    print_arr(array,length);
+    return 0;
+}
+
+
+void print_arr(int *arr, int n)
+{
+    for(int i = 0; i < n; ++i) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<'\n';
+}
+int* read_arr(int length)
+{
+    int *arr = new int[length];
+    for(int i = 0; i < length; ++i) {
+        cin>>arr[i];
+    }
+    return arr;
+}
